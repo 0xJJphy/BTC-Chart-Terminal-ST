08fcb8c5fea6628145d4f6778fecfc7ee0b30f7c
Pedro Alonso <xjjphy@gmail.com>
2025-12-24 00:34:33 +0100

    chore: add setup.bat

---
+++ setup.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing dependencies for BTC Chart Terminal...
+npm install
+echo.
+echo Dependencies installed successfully.
+echo You can now run the application with: npm run dev
+pause
